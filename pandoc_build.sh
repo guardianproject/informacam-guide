0fc2034711bc0c513e8241d192e15a06470aa933
n8fr8 <nathan@freitas.net>
2015-04-15 10:55:21 -0400

    add placeholder images for spanish, build script, and build output in HTML

---
+++ pandoc_build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pandoc -f markdown_strict -t html5 -o InformacamGuide.html About.txt chapter1.txt chapter2.txt chapter3.txt chapter4.txt chapter5.txt chapter6.txt chapter7.txt chapter8.txt chapter9.txt chapter10.txt chapter11.txt
