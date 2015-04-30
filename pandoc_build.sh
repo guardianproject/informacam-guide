7e25bae938d4dc0075bdf52d7e7a9b3a647a2c0d
n8fr8 <nathan@freitas.net>
2015-04-30 14:43:02 -0400

    update pandoc build script

--- pandoc_build.sh
+++ pandoc_build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 pandoc -f markdown_strict -t html5 -o InformacamGuide.html About.txt chapter1.txt chapter2.txt chapter3.txt chapter4.txt chapter5.txt chapter6.txt chapter7.txt chapter8.txt chapter9.txt chapter10.txt chapter11.txt
+pandoc -f markdown_strict -t odt -o InformacamGuide.odt About.txt chapter1.txt chapter2.txt chapter3.txt chapter4.txt chapter5.txt chapter6.txt chapter7.txt chapter8.txt chapter9.txt chapter10.txt chapter11.txt
+cat About.txt chapter1.txt chapter2.txt chapter3.txt chapter4.txt chapter5.txt chapter6.txt chapter7.txt chapter8.txt chapter9.txt chapter10.txt chapter11.txt > InformacamGuide.md
